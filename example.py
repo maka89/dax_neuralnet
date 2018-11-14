Fixed bug that ignored sign of certain NN weights

example.py
@@ -14,8 +14,8 @@ if __name__=="__main__":
 	#List of dicts
 	n_hidden=4
 	layers=[]
-	layers.append({"W":np.random.randn(len(input_features),n_hidden), "b":np.random.randn(1,n_hidden),"activation":"relu"})
-	layers.append({"W":np.random.randn(n_hidden,1), "b":np.random.randn(1,1),"activation":""})
+	layers.append({"W":np.random.randn(len(input_features),n_hidden), "b":np.random.randn(1,n_hidden),"activation":"tanh"})
+	layers.append({"W":np.random.randn(n_hidden,1), "b":np.random.randn(1,1),"activation":"sigmoid"})
 	
 	#Create NNDAX object
 	nnd=NNDAX(input_features,layers)

nn_dax.py
@@ -44,8 +44,12 @@ class NNDAX:
 			for j in range(0,layers[i]["W"].shape[1]):
 			
 				tmp=""
+				
 				for k in range(0,layers[i]["W"].shape[0]-1):
-					tmp+="{0:04e} * ".format(np.abs(layers[i]["W"][k,j])).replace(".",",") + "l_"+str(l-1)+"_"+str(k)
+					if k==0:
+						tmp+="{0:04e} * ".format(layers[i]["W"][k,j]).replace(".",",") + "l_"+str(l-1)+"_"+str(k)
+					else:
+						tmp+="{0:04e} * ".format(np.abs(layers[i]["W"][k,j])).replace(".",",") + "l_"+str(l-1)+"_"+str(k)
 					if layers[i]["W"][k+1,j] < 0.0:
 						tmp+= " - "
 					else:
@@ -64,6 +68,7 @@ class NNDAX:
 				str1+= self.assign_activation(tmp,layers[i]["activation"],l,j) + "\n"
 			if i != len(layers)-1:
 				str1+="\n\n"
+				
 		str1+= "\nreturn l_"+str(len(layers))+"_0"
 		return str1
 		